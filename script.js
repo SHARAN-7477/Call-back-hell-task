360738b8bfc6e81461f46e400b22a09b07660b70
SHARAN M <121308881+SHARAN-7477@users.noreply.github.com>
2023-01-31 11:36:26 +0530

    sorry for the delay submission

---
+++ script.js
@@ -0,0 +1,56 @@
+var set_time=document.querySelector("h1");
+setTimeout(()=>{
+    set_time.innerText='10'
+    document.body.style.color='#E32227';
+    document.body.style.backgroundColor='lightseagreen';
+        setTimeout(()=>{
+            set_time.innerText='9'
+            document.body.style.color='#E32227';
+            document.body.style.backgroundColor='lightseagreen';
+            setTimeout(()=>{
+                set_time.innerText='8'
+                document.body.style.color='#E32227';
+                document.body.style.backgroundColor='# EC9787';
+                setTimeout(()=>{
+                    set_time.innerText='7'
+                    document.body.style.color='#E32227';
+                    document.body.style.backgroundColor='#6EA1D4';
+                    setTimeout(()=>{
+                        set_time.innerText='6'
+                        document.body.style.color='#E32227';
+                        document.body.style.backgroundColor=' #6EA1D4';
+                        setTimeout(()=>{
+                            set_time.innerText='5'
+                            document.body.style.color='black';
+                            document.body.style.backgroundColor='#6EA1D4';
+                            setTimeout(()=>{
+                                set_time.innerText='4'
+                                document.body.style.color='black';
+                                document.body.style.backgroundColor='6EA1D4';
+                                setTimeout(()=>{
+                                    set_time.innerText='3'
+                                    document.body.style.color='#E32227';
+                                    document.body.style.backgroundColor=' #eb9687';
+                                    setTimeout(()=>{
+                                        set_time.innerText='2'
+                                        document.body.style.color='E32227';
+                                        document.body.style.backgroundColor='eb9687';
+                                        setTimeout(()=>{
+                                            set_time.innerText='1'
+                                            document.body.style.color='E32227';
+                                            document.body.style.backgroundColor='E32227';
+                                            setTimeout(()=>{
+                                                set_time.innerText='Happy Independence Day'
+                                                document.body.style.color='#E32227';
+                                                document.body.style.backgroundColor='rgb(56, 179, 127)';
+                                            },1000);
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+},1000);
